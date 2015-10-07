bfd8879fa2f66d0b07b623c873ba98835bf2d26f
saasbook <saasbook@saasbook.(none)>
2015-10-07 12:25:03 -0500

    First commit

---
+++ db/migrate/20151006155405_add_director_for_movies.rb
@@ -0,0 +1,5 @@
+class AddDirectorForMovies < ActiveRecord::Migration
+  def change
+    add_column :movies, :director, :string
+  end
+end
---
+++ features/step_definitions/movie_step.rb
@@ -0,0 +1,38 @@
+Given(/^the following movies exist:$/) do |table|
+  table.hashes.each do |movie|
+    Movie.create(movie)
+  end
+end
+
+Then(/^the director of "(.*?)" should be "(.*?)"$/) do |arg1, arg2|
+  assert page.body =~ /#{arg1}.+Director.+#{arg2}/m
+end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  page.body.index(e1).should < page.body.index(e2)
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(', ')
+  ratings.each do |rating|
+    if uncheck
+        step "I uncheck \"ratings_#{rating}\""
+      else
+        step "I check \"ratings_#{rating}\""
+      end
+  end
+end
+
+Then /I should see all the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+  page.body.scan(/<tr>/).length.should equal(Movie.all.length + 1)
+end
